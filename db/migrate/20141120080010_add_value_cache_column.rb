72fcc688e49539d49e65e6dfb87ac6c63b8c3ed2
Esu Chang <Esu@Esus-M.local>
2014-11-20 18:41:21 +0800

    updated the default value

--- db/migrate/20141120080010_add_value_cache_column.rb
+++ db/migrate/20141120080010_add_value_cache_column.rb
@@ -1,6 +1,6 @@
 class AddValueCacheColumn < ActiveRecord::Migration
   def change
-  	add_column :users, :value_cache, :integer
+  	add_column :users, :value_cache, :integer, :default 0
   	add_index :users, :value_cache
   end
 end
