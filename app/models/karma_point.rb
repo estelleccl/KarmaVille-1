b8526e3e9d3d5b5c2227ea29054a19e77dd1b57b
Esu Chang <Esu@Esus-M.local>
2014-11-20 18:23:39 +0800

    completed

--- app/models/karma_point.rb
+++ app/models/karma_point.rb
@@ -1,15 +1,25 @@
 class KarmaPoint < ActiveRecord::Base
-  attr_accessible :user_id, :label, :value
-  belongs_to :user
-  after_create :update_user
+ attr_accessible :user_id, :label, :value
+ belongs_to :user
+ before_save :get_value
+ after_save :update_change
 
-  validates :user, :presence => true
-  validates :value, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0}
-  validates :label, :presence => true
+ validates :user, :presence => true
+ validates :value, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0}
+ validates :label, :presence => true
 
-  def update_user
-   	user = User.find(user_id)
-   	user.total_karma += value
-   	user.save
-  end
-end
+ def get_value
+   	if id
+      @before_change = KarmaPoint.find(self.id).value
+   	else
+      @before_change = 0
+   	end
+ end
+
+ def update_change
+    change = value - @before_change
+    user = User.find(user_id)
+    user.total_karma += change
+    user.save
+ end
+ 
